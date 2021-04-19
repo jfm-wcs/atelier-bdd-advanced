956a3e21aa9b1f44272114effee0b4b508e514b8
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2021-04-19 14:43:58 +0200

    update index

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ define("TABLE_PERSON", "Person");
 define("TABLE_KINGDOM", "Kingdom");
 define("TABLE_ROLE", "Role");
 
-$query =  "SELECT AVG(p.age) AS average FROM " . TABLE_PERSON . " AS p LEFT JOIN " . TABLE_ROLE .  " AS r ON p.role_id = r.id WHERE r.role != 'magicien'";
+//write query here
+$query =  "";
 
 $statement = $pdo->query($query);
 var_dump($statement->fetch());
